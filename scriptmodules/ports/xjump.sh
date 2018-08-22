c37cacc9b92e6cbedb4daadee9f07262a956f643
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2018-08-22 10:36:38 -0700

    XSokoban, XJump, Fix for B.A.L.L.Z.

--- scriptmodules/ports/xjump.sh
+++ scriptmodules/ports/xjump.sh
@@ -9,33 +9,33 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="ballz"
-rp_module_desc="B.A.L.L.Z. - 2D Platformer"
+rp_module_id="xjump"
+rp_module_desc="XJump - Infinite tower climbing game"
 rp_module_licence="PROP"
 rp_module_section="exp"
 rp_module_flags="!mali !x86 !kms"
 
-function depends_ballz() {
-    getDepends xorg matchbox ballz-data
+function depends_xjump() {
+    getDepends xorg matchbox
 }
 
-function install_bin_ballz() {
-    [[ -f "$md_inst/scratch" ]] && rm -rf "$md_inst/"*
-    aptInstall scratch
+function install_bin_xjump() {
+    [[ -f "$md_inst/xjump" ]] && rm -rf "$md_inst/"*
+    aptInstall xjump
 }
 
-function remove_ballz() {
-    aptRemove scratch
+function remove_xjump() {
+    aptRemove xjump
 }
 
-function configure_ballz() {
-    addPort "$md_id" "ballz" "Ballz" "xinit $md_inst/ballz.sh"
+function configure_xjump() {
+    addPort "$md_id" "xjump" "XJump" "xinit $md_inst/xjump.sh"
 
-    cat >"$md_inst/ballz.sh" << _EOF_
+    cat >"$md_inst/xjump.sh" << _EOF_
 #!/bin/bash
 xset -dpms s off s noblank
 matchbox-window-manager &
-/usr/games/ballz
+/usr/games/xjump
 _EOF_
-    chmod +x "$md_inst/ballz.sh"
+    chmod +x "$md_inst/xjump.sh"
 }
--- scriptmodules/ports/xsokoban.sh
+++ scriptmodules/ports/xsokoban.sh
@@ -9,33 +9,33 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="ballz"
-rp_module_desc="B.A.L.L.Z. - 2D Platformer"
+rp_module_id="xsokoban"
+rp_module_desc="Xsokoban - Block Pushing Puzzle Game"
 rp_module_licence="PROP"
 rp_module_section="exp"
 rp_module_flags="!mali !x86 !kms"
 
-function depends_ballz() {
-    getDepends xorg matchbox ballz-data
+function depends_xsokoban() {
+    getDepends xorg matchbox
 }
 
-function install_bin_ballz() {
-    [[ -f "$md_inst/scratch" ]] && rm -rf "$md_inst/"*
-    aptInstall scratch
+function install_bin_xsokoban() {
+    [[ -f "$md_inst/xsokoban" ]] && rm -rf "$md_inst/"*
+    aptInstall xsokoban
 }
 
-function remove_ballz() {
-    aptRemove scratch
+function remove_xsokoban() {
+    aptRemove xsokoban
 }
 
-function configure_ballz() {
-    addPort "$md_id" "ballz" "Ballz" "xinit $md_inst/ballz.sh"
+function configure_xsokoban() {
+    addPort "$md_id" "xsokoban" "xsokoban" "xinit $md_inst/xsokoban.sh"
 
-    cat >"$md_inst/ballz.sh" << _EOF_
+    cat >"$md_inst/xsokoban.sh" << _EOF_
 #!/bin/bash
 xset -dpms s off s noblank
 matchbox-window-manager &
-/usr/games/ballz
+/usr/games/xsok
 _EOF_
-    chmod +x "$md_inst/ballz.sh"
+    chmod +x "$md_inst/xsokoban.sh"
 }
